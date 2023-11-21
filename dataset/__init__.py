33b158b3bb42ea4011539e4003d7d6668ad64673
weiaicunzai <372995411@qq.com>
2023-11-21 17:37:45 +0800

    feat: add conf

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,7 @@
 
 from .wsi import WSI
-# from .camlon16 import CAMLON16, CAMLON16Lable
-# from .utils import CAMLON16Lable
+from .wsi_reader import camlon16_wsis
+# # from .camlon16 import CAMLON16, CAMLON16Lable
+# # from .utils import CAMLON16Lable
+# from .dataloader import WSIDataLoader
+# from .dist_dataloader import DistWSIDataLoader

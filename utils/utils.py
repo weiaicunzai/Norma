bc465c9f6afe73dbbb688ac43d1b8e93bb0476fe
weiaicunzai <372995411@qq.com>
2023-11-27 19:07:08 +0800

    feat: comment out utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,26 @@
 
-import os
+# import os
 
-import glob
-import torch.distributed as dist
-from conf import camlon16
+# import glob
+# import torch.distributed as dist
+# # from conf import camlon16
 
-def init_process():
-    """ Initialize the distributed environment. """
-    rank = int(os.environ['LOCAL_RANK'])
-    size = int(os.environ['LOCAL_WORLD_SIZE'])
-    # os.environ['MASTER_PORT'] = '29500'
-    # dist.init_process_group(backend, rank=rank, world_size=size)
-    # print(size, '............................', os.environ["CUDA_VISIBLE_DEVICES"])
-    # dist.init_process_group('nccl', rank=rank, world_size=size)
-    dist.init_process_group('gloo', rank=rank, world_size=size)
-    # fn(rank, size)
+# def init_process():
+#     """ Initialize the distributed environment. """
+#     rank = int(os.environ['LOCAL_RANK'])
+#     size = int(os.environ['LOCAL_WORLD_SIZE'])
+#     # os.environ['MASTER_PORT'] = '29500'
+#     # dist.init_process_group(backend, rank=rank, world_size=size)
+#     # print(size, '............................', os.environ["CUDA_VISIBLE_DEVICES"])
+#     # dist.init_process_group('nccl', rank=rank, world_size=size)
+#     dist.init_process_group('gloo', rank=rank, world_size=size)
+#     # fn(rank, size)
 
 
-def cycle(iterable):
-    while True:
-        for data in iterable:
-            yield data
+# def cycle(iterable):
+#     while True:
+#         for data in iterable:
+#             yield data
 
 
 

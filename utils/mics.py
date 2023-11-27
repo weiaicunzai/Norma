6a3b5c570d55e3eec5c6f90bc4a0ee7f0cb583e0
weiaicunzai <372995411@qq.com>
2023-11-27 19:08:52 +0800

    feat: add compute_mean_and_std

--- utils/mics.py
+++ utils/mics.py
@@ -1,14 +1,15 @@
 
 import os
 import torch.distributed as dist
+import numpy as np
 
 
 
 
-def cycle(iterable):
-    while True:
-        for data in iterable:
-            yield data
+# def cycle(iterable):
+#     while True:
+#         for data in iterable:
+#             yield data
 
 
 def init_process():
@@ -21,3 +22,42 @@ def init_process():
     # dist.init_process_group('nccl', rank=rank, world_size=size)
     dist.init_process_group('gloo', rank=rank, world_size=size)
     # fn(rank, size)
+
+
+
+def compute_mean_and_std(dataset):
+    """Compute dataset mean and std, and normalize it
+    Args:
+        dataset: instance of torch.nn.Dataset
+    Returns:
+        return: mean and std of this dataset
+    """
+
+    mean = 0
+    std = 0
+
+    count = 0
+    print(len(dataset), 11)
+    # for img, _ in dataset:
+    for img in dataset:
+        img = img['img']
+        mean += np.mean(img, axis=(0, 1))
+        count += 1
+
+    mean /= len(dataset)
+
+    diff = 0
+    # for img, _ in dataset:
+    for img in dataset:
+        img = img['img']
+
+        diff += np.sum(np.power(img - mean, 2), axis=(0, 1))
+
+    # N = len(dataset) * np.prod(img.shape[:2])
+    N = len(dataset) * np.prod(img.size)
+    std = np.sqrt(diff / N)
+
+    mean = mean / 255
+    std = std / 255
+
+    return mean, std

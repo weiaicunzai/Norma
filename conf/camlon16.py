a23ba826509c8e17038a3a2d7ba997cc557c5d63
weiaicunzai <372995411@qq.com>
2023-11-28 18:32:50 +0800

    feat： add patch-level label dir

--- conf/camlon16.py
+++ conf/camlon16.py
@@ -30,7 +30,9 @@ train_dirs = {
     'wsis': [os.path.join(_dataset_path, 'training/normal/'), os.path.join(_dataset_path, 'training/tumor/')],
     # 'jsons': [os.path.join(_dataset_path, 'training_json/normal/patch_size_512_at_mag_5'), os.path.join(_dataset_path, 'training_json/tumor/patch_size_512_at_mag_5/')]
     'jsons': [os.path.join(_dataset_path, 'training_json/normal/patch_size_512_at_mag_20'), os.path.join(_dataset_path, 'training_json/tumor/patch_size_512_at_mag_20/')],
-    'lmdb' : [os.path.join(_dataset_path, 'training_lmdb')]
+    'lmdb' : [os.path.join(_dataset_path, 'training_lmdb')],
+    # 'lmdb' : ['/dev/shm/by/training_lmdb/'],
+    'patch_level': [os.path.join(_dataset_path, 'training_json/tumor/patch_size_512_at_mag_20_patch_label')]
 }
 
 # test_dirs = {
@@ -46,6 +48,8 @@ test_dirs = {
     # 'jsons' : [os.path.join(_dataset_path, 'testing/jsons/patch_size_512_at_mag_5/')]
     'jsons' : [os.path.join(_dataset_path, 'testing/jsons/patch_size_512_at_mag_20/')],
     'lmdb' : [os.path.join(_dataset_path, 'testing_lmdb/')],
+    # 'lmdb' : ['/dev/shm/by/testing_lmdb/'],
+    'patch_level': [os.path.join(_dataset_path, 'testing/jsons/patch_size_512_at_mag_20_patch_label')]
 }
 
 

f07f79e13d598520011e71f60402616cf424eef3
weiaicunzai <372995411@qq.com>
2024-01-04 17:25:58 +0800

    ref: rm comments

--- preprocess/create_patch_lmdb.py
+++ preprocess/create_patch_lmdb.py
@@ -16,7 +16,6 @@ sys.path.append(os.getcwd())
 
 
 def write_single_wsi(path, settings, q):
-# def write_single_wsi(path, settings):
 
     wsi_path, json_path = path
     t1 = time.time()
@@ -29,7 +28,6 @@ def write_single_wsi(path, settings, q):
 
     print('reading image {}'.format(wsi_path))
 
-    res = []
     for coord in coords[0]:
         (x, y), level, (patch_size_x, patch_size_y) = coord
         coord = (int(x), int(y)), int(level), (int(patch_size_x), int(patch_size_y))
@@ -49,18 +47,9 @@ def write_single_wsi(path, settings, q):
 
         img_byte_arr = io.BytesIO()
         patch.save(img_byte_arr, format='jpeg')
-        # print(patch.size)
-        # patch.save('test.jpg')
         img_byte_arr = img_byte_arr.getvalue()
         record = patch_id.encode(), img_byte_arr
-        # print(record)
         q.put(record)
-        #q.put((patch_id.encode(), img_byte_arr))
-        # res.append((patch_id.encode(), img_byte_arr))
-
-    # print(time.time() - t1, wsi_path)
-    # return res
-
 
     print(time.time() - t1, len(coords[0]), wsi_path)
 
@@ -81,44 +70,10 @@ def get_num_patch(path):
     json_data = json.load(open(json_path, 'r'))
     return len(json_data['coords'][0])
 
-    # base_name = json_data['filename']
-    # keys = []
-    # t1 = time.time()
-    # for coord in coords:
-    #     (x, y), level, (patch_size_x, patch_size_y) = coord
-
-    #     assert isinstance(x, int)
-    #     assert isinstance(y, int)
-    #     assert isinstance(level, int)
-    #     assert isinstance(patch_size_x, int)
-    #     assert isinstance(patch_size_y, int)
-
-    #     patch_id = '{basename}_{x}_{y}_{level}_{patch_size_x}_{patch_size_y}'.format(
-    #         basename=base_name,
-    #         x=x,
-    #         y=y,
-    #         level=level,
-    #         patch_size_x=patch_size_x,
-    #         patch_size_y=patch_size_y)
-
-    #     keys.append(patch_id)
-        # keys.append(patch_id)
-
-    # print(time.time() - t1, len(keys))
-    # return keys
-
-# def flatten(res):
-#     tmp = []
-#     for slide in res:
-#         for data in slide:
-#             tmp.append(data)
-
-#     return tmp
 def get_total_num_patches(settings):
     pool = mp.Pool(processes=mp.cpu_count())
     results = pool.map(get_num_patch, get_file_path(settings))
     return sum(results)
-
 
 def get_args_parser():
     parser = argparse.ArgumentParser(add_help=False)
@@ -178,123 +133,3 @@ if __name__ == '__main__':
             except Exception as e:
                 print(e)
                 break
-
-
-
-
-
-
-
-#    num_slides = 0
-#
-#    with env.begin(write=True) as txn:
-#
-#        proc = []
-#        pathes = []
-#        for path in get_file_path(settings):
-#
-#            pathes.append(path)
-#
-#            if len(pathes) == num_process:
-#                res = pool.map(fn, pathes)
-#                res = flatten(res)
-#
-#                for record in res:
-#                    txn.put(*record)
-#
-#                num_slides += len(pathes)
-#                print('processed {} slides'.format(num_slides))
-#                pathes = []
-#
-#        if pathes:
-#            res = pool.map(fn, pathes)
-#            res = flatten(res)
-#
-#            for record in res:
-#                txn.put(*record)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#            proc.append(
-#                Process(target=fn, args=(path,))
-#            )
-#
-#            if len(proc) == num_process:
-#                for p in proc:
-#                    p.start()
-#
-#                time.sleep(20)
-#                while True:
-#                    try:
-#                        # if after 10 seconds, still no data
-#                        # then means the process ends
-#                        record = q.get(timeout=10)
-#                        txn.put(*record)
-#                        count += 1
-#                        if count % 1000 == 0:
-#                            print('time', (time.time() - t1) / count, 'total', count)
-#                    except:
-#                        print('end of reading {} processes'.format(len(proc)))
-#                        break
-#
-#
-#                # wait untill process ends
-#                for p in proc:
-#                    p.join()
-#
-#                # clear prc
-#                proc = []
-#
-#        # if last p less len(proc)
-#        for p in proc:
-#            p.start()
-#
-#        time.sleep(10)
-#        while True:
-#            try:
-#                # if after 10 seconds, still no data
-#                # then means the process ends
-#                record = q.get(timeout=10)
-#                txn.put(*record)
-#                if count % 1000 == 0:
-#                    print('time', (time.time() - t1) / count, 'total', count)
-#            except:
-#                print('end of reading {} processes'.format(len(proc)))
-#                break
-#
-#        for p in proc:
-#            p.join()
-#

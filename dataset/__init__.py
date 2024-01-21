819f8ae6563287265a57b6752b5265c487d3a65e
weiaicunzai <372995411@qq.com>
2024-01-21 14:07:40 +0800

    rename dataset folder

--- dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .wsi import WSI, WSILMDB
-from .wsi_reader import CAMLON16MixIn
-# # from .camlon16 import CAMLON16, CAMLON16Lable
-# # from .utils import CAMLON16Lable
-# from .dataloader import WSIDataLoader
-# from .dist_dataloader import DistWSIDataLoader

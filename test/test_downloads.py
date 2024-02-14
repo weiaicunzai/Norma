668d1357f906023f437050c156e0e3e7a5278a11
weiaicunzai <372995411@qq.com>
2024-02-15 01:26:44 +0800

    feat: add multiprocessing support

--- test/test_downloads.py
+++ test/test_downloads.py
@@ -5,6 +5,7 @@ import glob
 import random
 import csv
 sys.path.append(os.getcwd())
+import multiprocessing
 
 import openslide
 
@@ -14,35 +15,37 @@ def get_wsi_path(settings):
         for row in csv.DictReader(f):
             yield os.path.join(settings.wsi_dir, row['slide_id'])
 
-def random_read(settings):
-    count = 0
-    for wsi_path in get_wsi_path(settings):
-        # print(wsi_path)
-        if not os.path.exists(wsi_path):
-            print('file {} do not exists'.format(wsi_path))
+def is_broken(wsi_path):
+    print('checking file {}'.format(wsi_path))
+    if not os.path.exists(wsi_path):
+        print('file {} do not exists'.format(wsi_path))
+        return True
+
+    wsi = openslide.OpenSlide(wsi_path)
+
+    for _ in range(30):
+        level = random.randint(0,len(wsi.level_dimensions)-1)
+        d1, d2 = wsi.level_dimensions[level]
+        d1 = random.randint(0, d1 - 256)
+        d2 = random.randint(0, d2 - 256)
+
+        try:
+            c = wsi.read_region((d1, d2), level, (512, 512))
+        except Exception as e:
+            print((d1, d2), level, (512, 512))
+            print(wsi)
+            print(e)
             count += 1
-            continue
+            # break
+            return True
 
-        # continue
-        wsi = openslide.OpenSlide(wsi_path)
+    return False
 
-        for _ in range(30):
-            level = random.randint(0,len(wsi.level_dimensions)-1)
-            d1, d2 = wsi.level_dimensions[level]
-            d1 = random.randint(0, d1 - 256)
-            d2 = random.randint(0, d2 - 256)
+def random_read(settings):
+    pool = multiprocessing.Pool(processes=4)
+    res = pool.map(is_broken, get_wsi_path(settings))
 
-            try:
-                c = wsi.read_region((d1, d2), level, (512, 512))
-            except Exception as e:
-                print((d1, d2), level, (512, 512))
-                print(wsi)
-                print(e)
-                count += 1
-                break
-
-    if count == 0:
-        print('no broken files are found')
+    print('total {} files are broken'.format(sum(res)))
 
 def get_args_parser():
     parser = argparse.ArgumentParser(add_help=False)
@@ -54,8 +57,10 @@ if __name__  == '__main__':
     args = get_args_parser()
     if args.dataset == 'brac':
         from conf.brac import settings
-    if args.dataset == 'bracs':
-        from conf.brac import settings
+    elif args.dataset == 'bracs':
+        from conf.bracs import settings
+    elif args.dataset == 'lung':
+        from conf.lung import settings
     else:
         raise ValueError('wrong value error')
 
